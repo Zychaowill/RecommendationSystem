9e5fd4c6543364016fa7e14daa20369772449d2c
Jang Zhang <jangz@synnex.com>
2017-11-16 16:35:24 +0800

    Finish Dao support operation

---
+++ recommendation-analysis/src/main/scala/com/jangz/recommendation/dao/DimensionDao.scala
@@ -0,0 +1,56 @@
+package com.jangz.recommendation.dao
+
+import com.jangz.recommendation.base.BasicSimpleDao
+import scala.collection.mutable.ListBuffer
+import com.jangz.recommendation.entity.Dimension
+
+object DimensionDao {
+
+  def getDimensionConfig(dimension: Dimension): ListBuffer[Dimension] = {
+    val sql: String = "SELECT id dimeId,`value` FROM common_dimension WHERE `type` = #{type}"
+    BasicSimpleDao.getSqlList(sql, dimension).asInstanceOf[ListBuffer[Dimension]]
+  }
+
+  def getDimensionConfigMap(dimension: Dimension): Map[String, Dimension] = {
+    val list: ListBuffer[Dimension] = getDimensionConfig(dimension)
+    var map: Map[String, Dimension] = Map[String, Dimension]()
+    for (dimension <- list) {
+      map += (dimension.value -> dimension)
+    }
+    map
+  }
+
+  def getSearchEngineMap(): Map[String, Dimension] = {
+    val dimension: Dimension = new Dimension
+    dimension.`type` = "Search-engine"
+    DimensionDao.getDimensionConfigMap(dimension)
+  }
+
+  def getCountryMap(): Map[String, Dimension] = {
+    val dimension: Dimension = new Dimension
+    dimension.`type` = "country"
+    DimensionDao.getDimensionConfigMap(dimension)
+  }
+
+  def getProvinceMap(): Map[String, Dimension] = {
+    val dimension: Dimension = new Dimension
+    dimension.`type` = "province"
+    DimensionDao.getDimensionConfigMap(dimension)
+  }
+
+  def saveDimensionData(dimension: Dimension): Int = {
+    val sql: String = "insert into sparkcore_dimension_data(dimeid,`day`,pv,uv,ip,time) values (#{dimeId},#{day},#{pv},#{uv},#{ip},#{time}) on duplicate key update pv = values(pv),uv = values(uv),ip = values(ip),time = values(time)"
+    BasicSimpleDao.saveObject(sql, dimension)
+  }
+
+  def saveDimensionList(list: List[Dimension]): Int = {
+    val sql: String = "insert into sparkcore_dimension_data(dimeid,`day`,pv,uv,ip,time) values (#{dimeId},#{day},#{pv},#{uv},#{ip},#{time}) on duplicate key update pv = values(pv),uv = values(uv),ip = values(ip),time = values(time)"
+    BasicSimpleDao.saveListBatch(sql, list)
+  }
+
+  def saveStreamingDimensionList(list: List[Dimension]): Int = {
+    val sql: String = "insert into streaming_dimension_data(dimeid,`second`,pv,uv) values (#{dimeId},#{second},#{pv},#{uv}) on duplicate key update pv = values(pv),uv = values(uv)"
+    BasicSimpleDao.saveListBatch(sql, list)
+  }
+
+}
---
+++ recommendation-analysis/src/main/scala/com/jangz/recommendation/dao/TrainingDao.scala
@@ -0,0 +1,17 @@
+package com.jangz.recommendation.dao
+
+import com.jangz.recommendation.entity.Training
+import com.jangz.recommendation.base.BasicSimpleDao
+
+object TrainingDao {
+
+  def saveGenderList(list: List[Training]): Int = {
+    val sql: String = "insert into mllib_gender_data(genderid,`day`,pv,uv,ip) values (#{genderId},#{day},#{pv},#{uv},#{ip}) on duplicate key update pv = values(pv),uv = values(uv),ip = values(ip)"
+    BasicSimpleDao.saveListBatch(sql, list)
+  }
+
+  def saveChannelList(list: List[Training]): Int = {
+    val sql: String = "insert into mllib_channel_data(channelid,`day`,pv,uv,ip) values (#{channelId},#{day},#{pv},#{uv},#{ip}) on duplicate key update pv = values(pv),uv = values(uv),ip = values(ip)"
+    BasicSimpleDao.saveListBatch(sql, list)
+  }
+}
