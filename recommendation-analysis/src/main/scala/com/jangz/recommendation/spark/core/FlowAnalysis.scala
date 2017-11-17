f2d2233de173b86afc36c0f841369630b8f187b0
Jang Zhang <jangz@synnex.com>
2017-11-17 08:57:14 +0800

    Finish analysis operation

---
+++ recommendation-analysis/src/main/scala/com/jangz/recommendation/spark/core/FlowAnalysis.scala
@@ -0,0 +1,62 @@
+package com.jangz.recommendation.spark.core
+
+import com.jangz.recommendation.config.Config
+import com.jangz.recommendation.util.SparkUtil
+import com.jangz.recommendation.entity.Dimension
+
+object FlowAnalysis {
+
+  def runAnalysis(): Unit = {
+    val sc = SparkUtil.getSparkContext(this.getClass)
+
+    val lines = sc.textFile(Config.input_path)
+    val filter = SparkUtil.getFilterLog(lines).cache()
+
+    /**
+     * pv & uv & ip by MR
+     */
+    val map = filter.map(log => {
+      val dimension: Dimension = new Dimension(pv = 1, uv = 1, ip = 1)
+      dimension.Ts = log.Ts
+      dimension.uvs += log.Uuid
+      dimension.ips += log.Ip
+      (dimension)
+    }).cache()
+
+    /**
+     * page views accumulate directly, user views and ip need to merge
+     */
+    val reduce = map.reduce((m, n) => {
+      m.pv += n.pv
+      m.uvs ++= n.uvs
+      m.ips ++= n.ips
+      m.uv = m.uvs.size
+      m.ip = m.ips.size
+      (m)
+    })
+
+    /**
+     * 计算时长(mapTime, reduceTime)
+     */
+    val mapTime = filter.map(log => (log.Uuid, log.Ts)).cache()
+
+    /**
+     * 1. 分组后对每个用户的访问时间排序
+     * 2. 遍历每个用户的访问时间，如果前后两次访问的时间差达到半小时（3600秒）就认为访问中断只加10秒
+     * 否则就加上两次访问的时间差
+     */
+    val reduceTime = mapTime.groupByKey().map(m => {
+      val list: List[Long] = m._2.toList.sorted
+      val time: Long = 10L
+      if (list.size > 1) {
+        
+      }
+    })
+  }
+
+  def main(args: Array[String]): Unit = {
+    val dayStr = if (args.length > 0) args(0) else "2016-12-01"
+    Config.setDay(dayStr)
+    runAnalysis()
+  }
+}
--- recommendation-analysis/src/main/scala/com/jangz/recommendation/spark/core/ProvinceAnalysis.scala
+++ recommendation-analysis/src/main/scala/com/jangz/recommendation/spark/core/ProvinceAnalysis.scala
@@ -1,56 +1,55 @@
 package com.jangz.recommendation.spark.core
 
+import com.jangz.recommendation.util.SparkUtil
+import com.jangz.recommendation.config.Config
+import com.jangz.recommendation.entity.Dimension
+import com.jangz.recommendation.dao.DimensionDao
 import org.apache.spark.broadcast.Broadcast
 
-import com.jangz.recommendation.config.Config
-import com.jangz.recommendation.dao.DimensionDao
-import com.jangz.recommendation.entity.Dimension
-import com.jangz.recommendation.util.SparkUtil
+object ProvinceAnalysis {
 
-object CountryAnalysis {
-  
   def runAnalysis(): Unit = {
     val sc = SparkUtil.getSparkContext(this.getClass)
-    
+
     val lines = sc.textFile(Config.input_path)
     val filter = SparkUtil.getFilterLog(lines).cache()
-    
-    val countryMap: Map[String, Dimension] = DimensionDao.getCountryMap()
-    val countryBroadCast: Broadcast[Map[String, Dimension]] = sc.broadcast(countryMap)
-    
+
+    val provinceMap: Map[String, Dimension] = DimensionDao.getProvinceMap()
+    val provinceBroadCast: Broadcast[Map[String, Dimension]] = sc.broadcast(provinceMap)
+
     /**
-     * calculate country distribution
+     * calculate province distribution
      */
     val map = filter.map(log => {
       val dimension: Dimension = new Dimension(pv = 1, uv = 1, ip = 1)
-      val country = log.Country
+      val area = log.Area
       dimension.uvs += log.Uuid
       dimension.ips += log.Ip
-      if (country != null) {
-        if (countryBroadCast.value.contains(country)) {
-          dimension.dimeId = countryBroadCast.value.get(country).get.dimeId
+      if (area != null) {
+        if (provinceBroadCast.value.contains(area)) {
+          dimension.dimeId = provinceBroadCast.value.get(area).get.dimeId
         }
       }
       (dimension.dimeId, dimension)
     }).filter(_._1 != 0).cache()
-    
+
     /**
      * pv & uv & ip by general ReduceByKey
      */
     val reduce = SparkUtil.getReduceByKey(map)
-    
+
     val list: List[Dimension] = reduce.values.collect().toList
     list.foreach(item => {
       item.day = Config.day
     })
     sc.stop()
-    
+
     /**
      * save into DB
      */
     DimensionDao.saveDimensionList(list)
   }
-  
+
   def main(args: Array[String]): Unit = {
     val dayStr = if (args.length > 0) args(0) else "2016-12-01"
     Config.setDay(dayStr)
